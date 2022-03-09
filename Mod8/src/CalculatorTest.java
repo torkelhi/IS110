6e2ba4ef79800bb1a218a4f5002a5599862a1eb0
torkelhi <torkelhi@uia.no>
2022-03-09 20:44:25 +0100

    refactored some code in calculator test

--- Mod8/src/CalculatorTest.java
+++ Mod8/src/CalculatorTest.java
@@ -1,24 +1,30 @@
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
 
+    private Calculator calculator;
+
+    @Before
+    public void setUp()
+    {
+    calculator = new Calculator();
+    }
+
     @Test
     public void calculatorInitialValueZero() {
-        Calculator calculator = new Calculator();
         assertEquals(0, calculator.getValue());
     }
 
     @Test
     public void valueFiveWhenFiveAdded() {
-        Calculator calculator = new Calculator();
         calculator.add(5);
         assertEquals(5, calculator.getValue());
     }
 
     @Test
     public void valueMinusTwoWhenTwoSubstracted() {
-        Calculator calculator = new Calculator();
         calculator.subtract(2);
         assertEquals(-2, calculator.getValue());
     }
