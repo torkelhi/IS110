150f24f7785267c5bfd1241c8b296904322eb196
torkelhi <torkelhi@uia.no>
2022-03-10 19:22:43 +0100

    optimalisert dokumentasjon i SykkelLager klassen

--- Mod09/src/SykkelLager.java
+++ Mod09/src/SykkelLager.java
@@ -27,6 +27,12 @@ public class SykkelLager {
     }
 
     //FilterFunksjoner
+
+    /**
+     * Metode som printer ut sykkelen dersom
+     * sykkelen sin pris er høyere en pris i param.
+     * @param pris
+     */
     public void printSykkelldyrereEnn(double pris)
     {
         Iterator<Sykkel> it = sykkel.iterator();
@@ -45,6 +51,12 @@ public class SykkelLager {
         }
     }
 
+
+    /**
+     * Metode som går gjennom Arraylisten ved og printer
+     * dersom String type er lik typen til sykkelen.
+     * @param type
+     */
     public void printSykkelEtterType(String type)
     {
         Iterator<Sykkel> it = sykkel.iterator();
